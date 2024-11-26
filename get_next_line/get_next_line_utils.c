7deed32dcc124cbb47d48d018bfdcf1da372a961
Basti_Wood <Sebwoody@gmail.com>
2024-11-26 14:17:48 +0100

    testing

--- get_next_line/get_next_line_utils.c
+++ get_next_line/get_next_line_utils.c
@@ -94,3 +94,82 @@ char	*ft_strdup(const char *str)
 	dup[i] = '\0';
 	return (dup);
 }
+
+////////////////////////////////////////////////////////////////////////////////////////////////////
+
+//eigene imlementierung
+
+#include <unistd.h>
+#include <stdlib.h>
+
+#ifndef BUFFER_SIZE
+# define BUFFER_SIZE 42
+#endif
+
+static char	*extract_line(char **buffer)
+{
+	char	*line;
+	char	*temp;
+	int		index;
+
+	index = 0;
+	while ((*buffer)[index] && (*buffer)[index] != '\n')
+		index++;
+	if ((*buffer)[index] == '\n')
+	{
+		line = ft_substr(*buffer, 0, index + 1);
+		temp = ft_strdup(&(*buffer)[index + 1]);
+		free(*buffer);
+		*buffer = temp;
+	}
+	else
+	{
+		line = ft_strdup(*buffer);
+		free(*buffer);
+		*buffer = NULL;
+	}
+	return (line);
+}
+
+static char	*read_to_buffer(int fd, char *buffer)
+{
+	char	*temp;
+	char	*read_buffer;
+	ssize_t	bytes_read;
+
+	read_buffer = malloc(BUFFER_SIZE + 1);
+	if (!read_buffer)
+		return (NULL);
+	while (!ft_strchr(buffer, '\n') && (bytes_read = read(fd, read_buffer, BUFFER_SIZE)) > 0)
+	{
+		read_buffer[bytes_read] = '\0';
+		temp = ft_strjoin(buffer, read_buffer);
+		free(buffer);
+		buffer = temp;
+	}
+	free(read_buffer);
+	if (bytes_read < 0)
+	{
+		free(buffer);
+		return (NULL);
+	}
+	return (buffer);
+}
+
+char	*get_next_line(int fd)
+{
+	static char	*buffer;
+
+	if (fd < 0 || BUFFER_SIZE <= 0)
+		return (NULL);
+	buffer = read_to_buffer(fd, buffer);
+	if (!buffer)
+		return (NULL);
+	if (*buffer == '\0')
+	{
+		free(buffer);
+		buffer = NULL;
+		return (NULL);
+	}
+	return (extract_line(&buffer));
+}
